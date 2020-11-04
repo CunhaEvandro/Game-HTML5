173bd765f6eac63b6405a56448324d78d9a9f060
EvandroCunha <sr.evanndro@gmail.com>
2020-11-03 21:47:50 -0300

    em desenvolvimento

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Game-HTML5
+Exemplo de jogo HTML5
---
+++ js/gravity.js
@@ -0,0 +1,21 @@
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+      this.gravitySpeed += this.gravity;
+      this.x += this.speedX;
+      this.y += this.speedY + this.gravitySpeed;
+    }
+  }
